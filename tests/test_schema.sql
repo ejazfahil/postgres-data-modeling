4295de4b17846005171c12738421fc042033a69d
Fahil Ejaz <ejazfahil@gmail.com>
2022-08-30 09:15:00 +0200

    test: add pgTAP schema tests

---
+++ tests/test_schema.sql
@@ -0,0 +1,7 @@
+-- pgTAP tests 2022-08-30
+SELECT plan(4);
+SELECT has_table('fact_sales');
+SELECT has_column('fact_sales','amount');
+SELECT col_not_null('fact_sales','quantity');
+SELECT has_index('fact_sales','idx_fact_sales_date');
+SELECT finish();
