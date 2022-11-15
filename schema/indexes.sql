6f1418a5609c75c713ac9ff90ee2716857d7a5f3
Fahil Ejaz <ejazfahil@gmail.com>
2022-11-15 10:30:00 +0100

    feat: add BRIN and partial indexes

--- schema/indexes.sql
+++ schema/indexes.sql
@@ -1,4 +1,4 @@
--- Indexes 2022-09-27
+-- Indexes 2022-11-15
 CREATE INDEX idx_fact_sales_date ON fact_sales USING BRIN (sold_at);
 CREATE INDEX idx_fact_sales_customer ON fact_sales (customer_id);
 CREATE INDEX idx_dim_product_sku ON dim_product (sku);
